a074a53dba20ab099f65524e7979a5b2c01afa5d
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-04-21 22:21:51 +0200

    doing some changes

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function downloadResultsAsJson(searchResults) {
   const downloadAnchorNode = document.createElement('a');
   downloadAnchorNode.setAttribute("href", dataStr);
   downloadAnchorNode.setAttribute("download", "search_results.json");
-  document.body.appendChild(downloadAnchorNode); // required for firefox
+  document.body.appendChild(downloadAnchorNode); 
   downloadAnchorNode.click();
   downloadAnchorNode.remove();
 }
@@ -51,25 +51,3 @@ function downloadSearchResults() {
   });
   downloadResultsAsJson(searchResults);
 }
-//"template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
-
-// rsz: filtered_cse
-// num: 10
-// hl: cs
-// source: gcsc
-// gss: .com
-// cselibv: 8435450f13508ca1
-// cx: 14a561912f073419f
-// q: david
-// safe: active
-// cse_tok: AB-tC_6wkDf1E5IktxMv9dwOB-8a:1713565728607
-// lr:
-// cr:
-// gl: cz
-// filter: 0
-// sort:
-// as_oq:
-// as_sitesearch:
-// exp: cc
-// fexp: 72519171,72519168
-// callback: google.search.cse.api11710
